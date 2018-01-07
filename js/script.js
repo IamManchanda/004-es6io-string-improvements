67856fbfa10fc7921d448524d32511a846e9cd28
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-07 16:37:57 +0530

    Additional String Improvements!

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/**
+ * JS
+ */
+
+const course = 'RFB2';
+const flightNumber = '20-AC2018-jz';
+const accountNumber = '825242631RT0001';
+
+const make = 'BMW';
+const model = 'x5';
+const color = 'Royal Blue';
+
+// .startsWith()
+
+// .endsWith()
+
+// .includes()
+
+// .repeat()
+
+function leftPad(str, length = 20) {
+  return `↪ ${' '.repeat(length - str.length)}${str}`;
+}
+
+console.log(leftPad(make));
+console.log(leftPad(model));
+console.log(leftPad(color));
